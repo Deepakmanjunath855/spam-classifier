3ea6125ba08fafcadaf484982a82beb75ac5246c
Deepak M <69420257+Deepakmanjunath855@users.noreply.github.com>
2021-05-15 17:42:06 +0530

    Add files via upload

---
+++ spam_classifier.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+messages = pd.read_csv('SMSSpamCollection', sep='\t',
+                           names=["label", "message"])
+
+#Data cleaning and preprocessing
+import re
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+corpus = []
+for i in range(0, len(messages)):
+    review = re.sub('[^a-zA-Z]', ' ', messages['message'][i])
+    review = review.lower()
+    review = review.split()
+    
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+    
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=2500)
+X = cv.fit_transform(corpus).toarray()
+
+y=pd.get_dummies(messages['label'])
+y=y.iloc[:,1].values
+
+
+# Train Test Split
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Training model using Naive bayes classifier
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred=spam_detect_model.predict(X_test)
+
+
+def predict(text):
+    labels=['Not spam','Spam']
+    f=cv.transform(text).toarray()
+    p=spam_detect_model.predict(f)
+    s=[str(i)for i in p]
+    v=int(''.join(s))
+    return str('This message is looking to be :' +labels[v])
+
+st.title('spam classifier')
+st.image('email-spam.png')
+userinput=st.text_input('write your message')
+submit=st.button('predict')
+if submit:
+    answer=predict([userinput])
+    st.text(answer)
+
+
+# In[ ]:
+
+
+
+
